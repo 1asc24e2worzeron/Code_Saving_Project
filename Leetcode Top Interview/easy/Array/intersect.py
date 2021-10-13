c820176ca537001a237b79211573ebe4637f1230
1asc24e2worzeron <bructw@gmail.com>
2021-10-13 14:08:53 +0800

    Create intersect.py
    寫這些的意義，大概在於熟悉原本沒有摸透的語言，並且更好的利用以得到寫出效率更高程式的能力
    至少有收穫，驗證了這不是浪費時間
    考試跟比賽的意義...應該也是驗證這兩方面吧，應用方面要履歷、作品還有實作競賽

---
+++ Leetcode Top Interview/easy/Array/intersect.py
@@ -0,0 +1,36 @@
+class Solution:
+    # 自己的第一種解法，別人跑很快，我寫就105ms
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums = []
+        for num in nums1:
+            if num in nums2:
+                nums.append(num)
+                nums2.remove(num)
+        return nums
+    
+    # 第二種，邏輯優化後的第一種，但還是比別人家的第一種64ms慢
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        count = 0
+        while count < len(nums1):
+            if nums1[count] in nums2:
+                nums2.remove(nums1[count])
+                count += 1
+            else:
+                nums1.remove(nums1[count])
+        return nums1
+        
+    # 看完學起來的最佳解
+    # 註解還開嘲諷，time: O(m+n)
+    # 重點是利用 collections.Counter 這個之前沒聽說的東西
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        # time: O(m+n), space: O(m+n)
+        counts = collections.Counter(nums1)
+        res = []
+
+        for num in nums2:
+            if counts[num] > 0:
+                res += num,
+                counts[num] -= 1
+
+        return res
