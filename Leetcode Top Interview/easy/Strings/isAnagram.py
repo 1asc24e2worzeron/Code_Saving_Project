5b24184ad9f9ee9d05ac8388819c72d994c988ff
1asc24e2worzeron <bructw@gmail.com>
2021-11-02 15:38:57 +0800

    Update isAnagram.py

--- Leetcode Top Interview/easy/Strings/isAnagram.py
+++ Leetcode Top Interview/easy/Strings/isAnagram.py
@@ -4,3 +4,7 @@ class Solution:
     # 看起來最快的幾群都是這個寫法，用 server 狀態決勝負
     def isAnagram(self, s: str, t: str) -> bool:
         return Counter(s) == Counter(t)
+    # 最快的(16ms)，有點不同，減小比對物件大小加速。順便學起來
+    # 看起來是先擷取其中要比對的部份，再用以創建累贅最少的 set 物件
+    def isAnagram(self, s: str, t: str) -> bool:
+        return set(Counter(s).items()) == set(Counter(t).items())
